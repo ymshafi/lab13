5b4c614ee09d3d6c89837a6eae32728149270d8f
Ian Edgeley <ian@iedgeley.com>
2014-10-30 14:15:06 -0500

    Updated with more instructions

--- 70-100pt.py
+++ 70-100pt.py
@@ -7,6 +7,7 @@
 
 # 70pt - Add buttons for left, right and down that move the player circle
 # 100pt - using lab 11 as an example, add in three horizontally scrolling "enemies"
+# Make them scroll at different speeds and directions.
 
 
 
@@ -16,28 +17,35 @@ root = Tk()
 drawpad = Canvas(root, width=800,height=600, background='white')
 player = drawpad.create_oval(390,580,410,600, fill="red")
 
+# Create your "enemies" here, before the class
+
+
 class MyApp:
 	def __init__(self, parent):
        	    global drawpad
        	    self.myParent = parent  
        	    self.myContainer1 = Frame(parent)
        	    self.myContainer1.pack()
-       	    self.button1 = Button(self.myContainer1)
-       	    self.button1.configure(text="up", background= "green")
-       	    self.button1.pack(side=LEFT)
-       	    self.button1.bind("<Button-1>", self.up)
+       	    self.up = Button(self.myContainer1)
+       	    self.up.configure(text="up", background= "green")
+       	    self.up.grid(row=0,column=0)
+       	    # Bind an event to the first button
+       	    self.up.bind("<Button-1>", self.upClicked)
+       	    
+       	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=BOTTOM)
 	
 	def animate(self):
 	    global drawpad
 	    global player
+	    # Remember to include your "enemies" with "global"
 	    	
 		
-	def up(self, event):   
+	def upClicked(self, event):   
 	   global oval
 	   global player
 	   drawpad.move(player,0,-20)
 		
 		
-myapp = MyApp(root)
+app = MyApp(root)
 root.mainloop()
